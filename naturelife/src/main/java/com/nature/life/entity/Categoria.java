b4b1a7c397f080e455a7b7619499fbbd2699cf3c
nmongiello <44971086+nmongiello@users.noreply.github.com>
2019-10-22 19:32:51 -0300

    nueva entidad

---
+++ naturelife/src/main/java/com/nature/life/entity/Categoria.java
@@ -0,0 +1,54 @@
+package com.nature.life.entity;
+
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "categoria")
+public class Categoria {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idCategoria",columnDefinition="bigint")
+	private BigInteger idCategoria;
+	
+	private String nombre;
+	
+	private String descripcion;
+		
+	public Categoria() {
+		
+	}
+	
+	public Categoria(BigInteger idCategoria, String nombre, String descripcion ) {
+		this.idCategoria = idCategoria;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+	
+	
+	
+}
